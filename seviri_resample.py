cb723534a6ee9d8b8c737c1cc0c48fb92cffb9b5
Pierre de Buyl <pdebuyl@pdebuyl.be>
2024-04-10 17:54:58 +0200

    change ash fields to 0..255 np.uint8

--- seviri_resample.py
+++ seviri_resample.py
@@ -5,11 +5,12 @@ import argparse
 import h5py
 import xarray as xr
 
+satpy.config.set(tmp_dir='/tmp')
 
 SEVIRI_CHANNELS = ['VIS006', 'VIS008', 'IR_016', 'IR_039', 'WV_062', 'WV_073', 'IR_087',
                    'IR_097', 'IR_108', 'IR_120', 'IR_134']
 
-ASH_CHANNELS = ['ash_red', 'ash_blue', 'ash_blue']
+ASH_CHANNELS = ['ash_red', 'ash_green', 'ash_blue']
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input', nargs='+')
@@ -24,6 +25,11 @@ if args.ash:
     SEVIRI_CHANNELS = SEVIRI_CHANNELS + ASH_CHANNELS
 
 sev_scene.load(SEVIRI_CHANNELS)
+
+sev_scene['ash_red'].data = ( (sev_scene['ash_red'].data + 4)/6 * 255).clip(0, 255).astype(np.uint8)
+sev_scene['ash_green'].data = ( (sev_scene['ash_green'].data + 4)/9 * 255).clip(0, 255).astype(np.uint8)
+sev_scene['ash_blue'].data = ( (sev_scene['ash_blue'].data - 243)/(303.-243.) * 255).clip(0, 255).astype(np.uint8)
+
 if args.cobra:
     # add extra component
     copy_attrs = {'start_time': sev_scene['VIS006'].attrs['start_time'], 'area': sev_scene['VIS006'].attrs['area'], 'standard_name': 'COBRA Ice'}
