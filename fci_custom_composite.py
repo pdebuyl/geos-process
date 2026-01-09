e507dbf8feac7b0467c8e01bdecea057d70ef919
Pierre de Buyl <pdebuyl@pdebuyl.be>
2026-01-09 10:22:20 +0100

    add in-line SBAF feature for ash composite

--- fci_custom_composite.py
+++ fci_custom_composite.py
@@ -1,11 +1,25 @@
 import argparse
 import glob
 import hdf5plugin
+import json
 import os
 import pathlib
 import pyresample
+import xarray as xr
 
-def custom_ash(input_scene, query_dict, standard_name='ash'):
+
+def sbaf_single(band, coefs):
+    """
+    Apply a polynomial SBAF correction.
+    """
+    data = coefs[1]*band.data + coefs[0]
+    for i, coef in enumerate(coefs[2:]):
+        data += coef*band.data**(i+2)
+    res = xr.DataArray(data, dims=band.dims, attrs=band.attrs, coords=band.coords)
+    return res
+
+
+def custom_ash(input_scene, query_dict, standard_name='ash', sbaf_coefs=None):
     """
     Create a custom ash composite based on the DataQuery entries in `query_dict`.
     """
@@ -14,14 +28,26 @@ def custom_ash(input_scene, query_dict, standard_name='ash'):
     ash_g_compositor = satpy.composites.arithmetic.DifferenceCompositor("ash_g")
     ash_compositor = satpy.composites.core.GenericCompositor("ash")
 
-    r = ash_r_compositor([input_scene[queries['r'][0]], input_scene[queries['r'][1]]])
-    g = ash_g_compositor([input_scene[queries['g'][0]], input_scene[queries['g'][1]]])
-    b = input_scene[queries['b'][0]]
+    r_123 = input_scene[queries['r'][0]]
+    r_105 = input_scene[queries['r'][1]]
+    g_105 = input_scene[queries['g'][0]]
+    g_087 = input_scene[queries['g'][1]]
+    b_105 = input_scene[queries['b'][0]]
+    if sbaf_coefs is not None:
+        r_123 = sbaf_single(r_123, sbaf_coefs['ir_123'])
+        r_105 = sbaf_single(r_105, sbaf_coefs['ir_105'])
+        g_105 = sbaf_single(g_105, sbaf_coefs['ir_105'])
+        g_087 = sbaf_single(g_087, sbaf_coefs['ir_87'])
+        b_105 = sbaf_single(b_105, sbaf_coefs['ir_105'])
+    r = ash_r_compositor([r_123, r_105])
+    g = ash_g_compositor([g_105, g_087])
+    b = b_105
 
     ash = ash_compositor([r, g, b])
     ash.attrs['standard_name'] = standard_name
 
     return ash
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-r', nargs=2, default=(2000, 2000), type=int)
@@ -31,6 +57,7 @@ parser.add_argument('--out', required=True)
 parser.add_argument('--data', required=True)
 parser.add_argument('--region', default='econtrail_002')
 parser.add_argument('--standard-name', default='ash')
+parser.add_argument('--sbaf', action='store_true')
 args = parser.parse_args()
 
 etc = pathlib.Path(__file__).parent / 'etc_custom_ash_wmo'
@@ -38,23 +65,33 @@ os.environ['SATPY_CONFIG_PATH'] = etc.as_posix()
 
 import satpy
 import satpy.composites.arithmetic
+import satpy.modifiers
+import geos_utils
+
 
 # Define area definition for region of interest
-ad = pyresample.load_area("/home/pierre/code/geos-process/RSS_areas.yaml", args.region)
+ad = pyresample.load_area("RSS_areas.yaml", args.region)
+if args.sbaf:
+    with open("goes_fci_ash_bands.json", 'r') as f:
+        coefs = json.load(f)
+else:
+    coefs = None
 
 # Create queries per RGB channel and wavelength
 queries = {'r': [], 'g': [], 'b': []}
 ash_wl = {'r': (12.3, 10.5), 'g': (10.5, 8.7), 'b': (10.5,)}
-for channel in ash_wl.keys():
-    for wl, resolution in zip(ash_wl[channel], getattr(args, channel)):
-        q = satpy.DataQuery(wavelength=wl, resolution=resolution)
+ash_name = {'r': ('ir_123', 'ir_105'), 'g': ('ir_105', 'ir_87'), 'b': ('ir_105',)}
+for channel in ash_name.keys():
+    for name, resolution in zip(ash_name[channel], getattr(args, channel)):
+        q = satpy.DataQuery(name=name, resolution=resolution)
         queries[channel].append(q)
 
 # Create the scene, load the queries, resample, and call the custom ash composite
 fn = glob.glob(args.data + '/W*FCI*BODY*.nc')
 scn = satpy.Scene(filenames=fn, reader='fci_l1c_nc')
+
 scn.load([*queries['r'], *queries['g'], *queries['b']])
 scn_2 = scn.resample(ad, resampler='gradient_search')
-scn_2['ash'] = custom_ash(scn_2, queries, args.standard_name)
+scn_2['ash'] = custom_ash(scn_2, queries, args.standard_name, sbaf_coefs=coefs)
 
 scn_2.save_dataset('ash', writer='simple_image', filename=args.out)
